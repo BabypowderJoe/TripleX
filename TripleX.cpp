b5fda42fc868f1e518afb3dcdffe2a62dd05ff80
Joseph Sozio <josephmsozio@gmail.com>
2020-05-19 01:54:06 -0400

    Init

---
+++ TripleX.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() 
+{ 
+    std::cout << "Your wife working at the cable company is trying to send you messages through your TV";
+    std::cout << std::endl;
+    std::cout << "You need to find the correct channels to continue...";
+
+    int a = 4;
+    int b = 8;
+    int c = 18;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
+    return 0;
+}
