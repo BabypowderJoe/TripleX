f5144906add5402169133be4d3432db759a84397
Joseph Sozio <josephmsozio@gmail.com>
2020-05-21 21:11:07 -0400

    Lectures 20-22

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,17 +2,20 @@
 
 int main() 
 { 
+    // Intro Story and Player Goal
     std::cout << "Your wife working at the cable company is trying to send you messages through your TV";
     std::cout << std::endl;
     std::cout << "You need to find the correct channels to continue...";
 
-    int a = 4;
-    int b = 8;
-    int c = 18;
+    // Declare 3 number code
+    const int a = 4;
+    const int b = 8;
+    const int c = 18;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
+    // Print sum and product to the terminal
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
