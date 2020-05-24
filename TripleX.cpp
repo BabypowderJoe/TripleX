2a071ffa31619b194937045671d8df3082b1b43e
Joseph Sozio <josephmsozio@gmail.com>
2020-05-23 22:44:11 -0400

    Lectures 23 and 24

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,22 +3,31 @@
 int main() 
 { 
     // Intro Story and Player Goal
-    std::cout << "Your wife working at the cable company is trying to send you messages through your TV";
+    std::cout << "Your wife working at the cable company is trying to send you a message through your TV";
     std::cout << std::endl;
-    std::cout << "You need to find the correct channels to continue...";
+    std::cout << "You need to find the correct channel to continue..." << std::endl;
 
     // Declare 3 number code
-    const int a = 4;
-    const int b = 8;
-    const int c = 18;
+    const int CodeA = 4;
+    const int CodeB = 8;
+    const int CodeC = 18;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    // Print sum and product to the terminal
+    // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "There are 3 numbers in the channel" << std::endl;
+    std::cout << "The numbers add up to: " << CodeSum << std::endl;
+    std::cout << "The numbers multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct= GuessA * GuessB * GuessC;
 
     return 0;
 }
